1bae410c73b9767420950cafa26c89b2fb2a85b8
Christopher A. Stelma <cstelma@tagged.com>
2014-05-23 14:17:35 -0700

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+def load_requirements(fname):
+    #TODO: use pkg_resources to grab the file (in case we're inside an archive)
+    with open(fname, 'r') as reqfile:
+        reqs = reqfile.read()
+
+    return filter(None, reqs.strip().splitlines())
+
+REQUIREMENTS = load_requirements('requirements.txt')
+
+setup(
+    name='flaskapi',
+    version='0.1',
+    package_dir = {'': 'src'},
+    packages = find_packages(),
+
+    install_requires=REQUIREMENTS,
+    entry_points = {
+        'console_scripts': [
+            'brocade-tool = brocadetool.brocadetool:main',
+            ]
+    },
+)
